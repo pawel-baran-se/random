e475ba0f6813b6c866427494e2c03a11e8861ed6
pawel-baran-se <pawelbaranc@gmail.com>
2022-02-24 21:47:03 +0100

    class manipulate added

---
+++ AutoCadPlugIn_Exercises_/AutoCAD_manipulate_01/AutoCAD_manipulate_01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCAD_manipulate_01
+{
+    public class AutoCAD_manipulate_01
+    {
+    }
+}
