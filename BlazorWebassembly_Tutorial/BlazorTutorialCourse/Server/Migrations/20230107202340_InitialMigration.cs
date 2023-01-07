4b660cd5c0c975e36feed9a95aecfe3ef9ed7e0b
Pawel Baran <pawelbaranc@gmail.com>
2023-01-07 21:39:38 +0100

    dataBase ReadyToUse

---
+++ BlazorWebassembly_Tutorial/BlazorTutorialCourse/Server/Migrations/20230107202340_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ThumbnailImagePath = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "Name", "ThumbnailImagePath" },
+                values: new object[,]
+                {
+                    { 1, "A description of category 1", "Category 1", "uploads/placeholder.jpg" },
+                    { 2, "A description of category 2", "Category 2", "uploads/placeholder.jpg" },
+                    { 3, "A description of category 3", "Category 3", "uploads/placeholder.jpg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
