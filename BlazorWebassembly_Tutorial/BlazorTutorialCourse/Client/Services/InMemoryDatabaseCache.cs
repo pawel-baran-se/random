238ce37e53bbe3020f2bbdf83b045980db53214d
Pawel Baran <pawelbaranc@gmail.com>
2023-01-07 22:12:54 +0100

    Consuming API 1/2

---
+++ BlazorWebassembly_Tutorial/BlazorTutorialCourse/Client/Services/InMemoryDatabaseCache.cs
@@ -0,0 +1,31 @@
+﻿using Shared.Models;
+
+namespace Client.Services
+{
+	internal sealed class InMemoryDatabaseCache
+	{
+		private readonly HttpClient _httpClient;
+
+		public InMemoryDatabaseCache(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		private List<Category> _categories = null;
+
+		internal List<Category> Categories
+		{
+			get { return _categories; }
+			set
+			{
+				_categories = value;
+				NotifyCategoriesDataChanged();
+			}
+		}
+
+		internal event Action OnCategoriesDataChanged;
+
+		private void NotifyCategoriesDataChanged() => OnCategoriesDataChanged?.Invoke();
+
+	}
+}
