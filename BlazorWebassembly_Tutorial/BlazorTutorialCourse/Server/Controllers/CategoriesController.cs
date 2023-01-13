da48212b71ba133103046e0f2ff801d5da1f3c57
Pawel Baran <pawelbaranc@gmail.com>
2023-01-13 18:19:12 +0100

    CategoryController Updated

--- BlazorWebassembly_Tutorial/BlazorTutorialCourse/Server/Controllers/CategoriesController.cs
+++ BlazorWebassembly_Tutorial/BlazorTutorialCourse/Server/Controllers/CategoriesController.cs
@@ -19,10 +19,72 @@ namespace Server.Controllers
 
 
 		[HttpGet]
-		public async Task<IActionResult> Get() {
+		public async Task<IActionResult> Get()
+		{
 
 			List<Category> categories = await _appDBContext.Categories.ToListAsync();
 			return Ok(categories);
+		}
+
+		[HttpGet("withposts")]
+		public async Task<IActionResult> GetWithPosts(bool withPosts)
+		{
+
+			List<Category> categories = await _appDBContext.Categories
+				.Include(c => c.Posts)
+				.ToListAsync();
+
+			return Ok(categories);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Category>> GetById(int id)
+		{
+
+			Category category = await GetCategoryByCategoryId(id, false);
+
+			return Ok(category);
+		}
+
+		[HttpGet("withposts/{id}")]
+		public async Task<ActionResult<Category>> GetByIdWithPost(int id)
+		{
+
+			Category category = await GetCategoryByCategoryId(id, true);
+
+			return Ok(category);
+		}
+
+
+
+		[NonAction]
+		[ApiExplorerSettings(IgnoreApi = true)]
+		private async Task<bool> PersistChangesToDatabase()
+		{
+			int amountOfChanges = await _appDBContext.SaveChangesAsync();
+
+			return amountOfChanges > 0;
+		}
+
+		[NonAction]
+		[ApiExplorerSettings(IgnoreApi = true)]
+		private async Task<Category> GetCategoryByCategoryId(int categoryId, bool withPosts)
+		{
+			Category categoryToGet = null;
+
+			if (withPosts == true)
+			{
+				categoryToGet = await _appDBContext.Categories
+					.Include(c => c.Posts)
+					.FirstAsync(c => c.CategoryId == categoryId);
+			}
+			else
+			{
+				categoryToGet = await _appDBContext.Categories
+					.FirstAsync(c => c.CategoryId == categoryId);
+			}
+
+			return categoryToGet;
 		}
 	}
 }
