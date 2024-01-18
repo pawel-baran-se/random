26e8cab78b86ae8f2cc12b6ecbe6d8186563dbe9
Pawel Baran <pawelbaranc@gmail.com>
2024-01-18 22:00:47 +0100

    Added YarpProxy

--- YarpProject/YaProxy/Program.cs
+++ YarpProject/YaProxy/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//ReverseProxy
+builder.Services.AddReverseProxy()
+	.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapReverseProxy();
 
 app.Run();
